18473c7017009ee78391d3edd5464fe82163c70f
Christian Legge <christian@christianlegge.dev>
2023-09-21 20:16:02 -0400

    more progress

--- runelite-client/src/main/java/net/runelite/client/plugins/wikilinks/WikiLinksPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/wikilinks/WikiLinksPlugin.java
@@ -28,10 +28,21 @@ package net.runelite.client.plugins.wikilinks;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.Experience;
+import net.runelite.api.IconID;
+import net.runelite.api.MessageNode;
 import net.runelite.api.Skill;
 import net.runelite.api.events.ChatMessage;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.widgets.WidgetPositionMode;
+import net.runelite.api.widgets.WidgetType;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.events.ConfigChanged;
 import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.client.callback.ClientThread;
@@ -40,6 +51,7 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.PluginChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.QuantityFormatter;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -54,7 +66,7 @@ import java.util.regex.Pattern;
 public class WikiLinksPlugin extends Plugin
 {
 	private static final String TOTAL_LEVEL_TEXT_PREFIX = "Total level:<br>";
-	private final Pattern LINK_PATTERN = Pattern.compile("\\[\\[([\\w\\s]+)\\]\\]");
+	private final Pattern LINK_PATTERN = Pattern.compile("\\[\\[(\\w[\\w\\s\\/'-]*)\\]\\]");
 
 	@Inject
 	private WikiLinksConfig config;
@@ -64,6 +76,9 @@ public class WikiLinksPlugin extends Plugin
 
 	@Inject
 	private ClientThread clientThread;
+
+	@Inject
+	private ChatMessageManager chatMessageManager;
 
 	@Provides
 	WikiLinksConfig provideConfig(ConfigManager configManager)
@@ -90,7 +105,7 @@ public class WikiLinksPlugin extends Plugin
 	@Subscribe
 	public void onConfigChanged(ConfigChanged configChanged)
 	{
-		if (!configChanged.getGroup().equals("virtuallevels"))
+		if (!configChanged.getGroup().equals("wikilinks"))
 		{
 			return;
 		}
@@ -100,10 +115,76 @@ public class WikiLinksPlugin extends Plugin
 
 	@Subscribe
 	public void onChatMessage(ChatMessage chatMessage) {
-		Matcher m = LINK_PATTERN.matcher(chatMessage.getMessage());
-		while (m.find()) {
-			log.debug(m.group(1));
+		final ChatMessageType messageType = chatMessage.getType();
+		if (messageType != ChatMessageType.PUBLICCHAT) {
+			return;
 		}
+
+//		Matcher m = LINK_PATTERN.matcher(chatMessage.getMessage());
+
+		final MessageNode node = chatMessage.getMessageNode();
+
+		final String linked = chatMessage.getMessage().replace("\\[\\[(\\w[\\w\\s\\/'-]*)\\]\\]", "<query=https://google.com><u><colHIGHLIGHT><img=12>$1</u><colNORMAL>");
+		node.setRuneLiteFormatMessage(linked);
+
+		Widget chatbox = client.getWidget(WidgetInfo.CHATBOX_MESSAGE_LINES);
+
+		Widget a = chatbox.createChild(-1, WidgetType.RECTANGLE);
+		a.setTextColor(0x113399);
+		a.setFilled(true);
+		a.setOriginalX(0);
+		a.setOriginalY(0);
+		a.setXPositionMode(WidgetPositionMode.ABSOLUTE_CENTER);
+		a.setYPositionMode(WidgetPositionMode.ABSOLUTE_CENTER);
+		a.setOriginalWidth(200);
+		a.setOriginalHeight(20);
+		a.revalidate();
+
+		return;
+
+
+//		while (m.find()) {
+//			final StringBuilder builder = new StringBuilder()
+//					.append("my test message ")
+//					.append("<u>")
+//					.append("<col")
+//					.append(ChatColorType.HIGHLIGHT.name())
+//					.append(">")
+//					.append("<img=")
+//					.append(IconID.CHAIN_LINK.getIndex())
+//					.append(">")
+//					.append("link")
+//					.append("</u>")
+//					.append("<col")
+//					.append(ChatColorType.NORMAL.name())
+//					.append(">")
+//					.append(" hello");
+//
+//			final ChatMessageBuilder msgBuilder = new ChatMessageBuilder()
+//					.append("my test message ")
+//					.append(ChatColorType.HIGHLIGHT)
+//					.img(IconID.CHAIN_LINK.getIndex())
+//					.append("link")
+//					.append(ChatColorType.NORMAL)
+//					.append(" hello");
+//
+//			log.debug(builder.toString());
+//			log.debug(msgBuilder.build());
+//			log.debug(chatMessage.getMessage());
+//
+//
+//			node.setRuneLiteFormatMessage(linked);
+////			chatMessageManager.queue(QueuedMessage.builder()
+////					.type(chatMessage.getType())
+////					.runeLiteFormattedMessage(builder.toString())
+////					.build());
+//			log.debug(m.group(1));
+//			log.debug("matched 1");
+//		}
+	}
+
+	private void onClick(Widget w) {
+
 	}
 
 	@Subscribe
